def93ffd3ecde6b3ef2b9ec261aab14b10539e0e
ORIENTSOFTWARE\hai.le <ledaihai.160500@gmail.com>
2023-02-09 09:43:55 +0700

    abc

---
+++ MoneyHeist/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyHeist.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+    }
+}
